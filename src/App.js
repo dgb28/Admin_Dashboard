86648a1d8aebd91d600ca3e1b993f4f9903f4907
dgb28 <85788484+dgb28@users.noreply.github.com>
2023-05-24 20:35:47 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext,useMode } from "./theme";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import { Route,Routes } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard  from "./scenes/dashboard";
+import { MyProSidebarProvider } from "./scenes/global/sidebar/sidebarContext";
+// import Sidebar from "./scenes/global/Sidebar";
+// import  Team  from "./scenes/Team";
+// import  Invoices  from "./scenes/invoices";
+// import  Contacts  from "./scenes/contacts";
+// import  Bar  from "./scenes/bar";
+// import  Form  from "./scenes/form";
+// import  Pie  from "./scenes/pie";
+// import  Line  from "./scenes/line";
+// import  FAQ  from "./scenes/faq";
+// import  Geography  from "./scenes/geography";
+// import  Calendar  from "./scenes/calendar";
+
+
 
 function App() {
+  const [theme,colorMode]=useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <MyProSidebarProvider>
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+              {/* <Route path="/team" element={<Team/>}/> */}
+              {/* <Route path="/invoices" element={<Invoices/>}/> */}
+              {/* <Route path="/contacts" element={<Contacts/>}/> */}
+              {/* <Route path="/bar" element={<Bar/>}/> */}
+              {/* <Route path="/form" element={<Form/>}/> */}
+              {/* <Route path="/pie" element={<Pie/>}/> */}
+              {/* <Route path="/line" element={<Line/>}/> */}
+              {/* <Route path="/faq" element={<FAQ/>}/> */}
+              {/* <Route path="/geography" element={<Geography/>}/> */}
+              {/* <Route path="/calendar" element={<Calendar/>}/> */}
+            </Routes>
+          </main>
+        </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Headers"
+
+const Dashboard=()=>{
+    return <Box display="flex" justifyContent="space-between" align-items="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard"/>
+    </Box>
+}
+export default Dashboard;
